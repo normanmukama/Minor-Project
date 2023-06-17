b493a0806b20fb268915d2ffb3ae6e6cbe0f3b8b
Norman-dev <normanmukama11@gmail.com>
2023-06-17 18:19:22 +0300

    deleted some files

--- Dashboard/main_dash.php
+++ Dashboard/main_dash.php
@@ -508,13 +508,13 @@ if (isset($_SESSION['username'])) {
                                             </div>
                                         <div class="mt-4 text-center small">
                                             
-                                            <div class="progress my-2">
+                                            <div class="progress my-4">
                                                <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-label="Animated striped example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%;"></div>
                                             </div>
-                                            <div class="progress my-2">
+                                            <div class="progress my-4">
                                                <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-label="Animated striped example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 50%;"></div>
                                             </div>
-                                            <div class="progress my-2">
+                                            <div class="progress my-4">
                                                <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-label="Animated striped example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style="width: 25%;"></div>
                                             </div>
                                             <div class="mt-4">
