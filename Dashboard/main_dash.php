1910bee2e01479f8aa6d218644749bc336e3bcd7
Norman-dev <normanmukama11@gmail.com>
2023-06-26 09:41:05 +0300

    added the final remarks

--- Dashboard/main_dash.php
+++ Dashboard/main_dash.php
@@ -99,13 +99,13 @@ if (isset($_SESSION['username'])) {
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                                 aria-labelledby="userDropdown">
-                                <a class="dropdown-item" href="#">
+                                <!-- <a class="dropdown-item" href="#">
                                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Profile
                 
-                                </a>
+                                </a> -->
                                 
-                                    <a class="dropdown-item" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
+                                    <!-- <a class="dropdown-item" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
                                       <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>        
                                       settings
                                     </a>
@@ -113,7 +113,7 @@ if (isset($_SESSION['username'])) {
                                 <a class="dropdown-item" href="#">
                                     <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Activity Log
-                                </a>
+                                </a> -->
                                 <div class="dropdown-divider"></div>
                                 <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
---
+++ readme.txt
@@ -0,0 +1,9 @@
+The name of the database is minor_project
+The admin name is Norman
+The admin password is admin
+
+
+//To test for the user
+
+Email : normanmukama11@gmail.com
+password: 12345
